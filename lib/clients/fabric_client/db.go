423196ad506bfdfc222ef735838b8f2ca4bcbf82
GaffarovDaniel <daniel.gaffarov@mail.ru>
2025-10-28 23:35:13 +0800

    Changing the constructor

--- lib/clients/fabric_client/db.go
+++ lib/clients/fabric_client/db.go
@@ -2,18 +2,24 @@ package fabricClient
 
 import (
 	"context"
-	clientsDB "lib/clients/db"
+	"fmt"
 	clickhouseClient "lib/clients/db/clickhouse"
 	redisClient "lib/clients/db/redis"
 	"lib/models"
 )
 
-// NewClickhouse создает клиента ClickHouse по конфигу
-func NewClickhouse(ctx context.Context, cfg models.Clickhouse) (clientsDB.ClickhouseClient, error) {
-	return clickhouseClient.NewClient(ctx, cfg)
-}
+const (
+	dbTypeClickhouse = "clickhouse"
+	dbTypeRedis      = "redis"
+)
 
-// NewCash создает клиента Redis по конфигу
-func NewCash(ctx context.Context, cfg models.Redis) (clientsDB.CashClient, error) {
-	return redisClient.NewClient(ctx, cfg)
+func NewDB(ctx context.Context, dbType string, cfg models.DB) (any, error) {
+	switch dbType {
+	case dbTypeClickhouse:
+		return clickhouseClient.NewClient(ctx, models.Clickhouse{DB: cfg})
+	case dbTypeRedis:
+		return redisClient.NewClient(ctx, models.Redis{DB: cfg})
+	default:
+		return nil, fmt.Errorf("not found db client for type: %s", dbType)
+	}
 }
